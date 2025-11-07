3a47224fe0a9b98b839a53361582e32718d04222
Arnab Mallick <arnab2239@gmail.com>
2025-11-07 12:53:34 +0530

    Create security_group.tf

---
+++ terraform/security_group.tf
@@ -0,0 +1,41 @@
+resource "aws_security_group" "allow_user_to_connect" {
+  name        = "allow_tls"
+  description = "Allow user to connect"
+  vpc_id      = module.vpc.vpc_id
+  ingress {
+    description = "Allow SSH (22)"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP (80)"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS (443)"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+
+  egress {
+    description = "Allow all outgoing traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "mysecurity"
+  }
+}
