4f2d86d3e8c793c1420929c76a3fdfef1f442c79
Arnab Mallick <arnab2239@gmail.com>
2025-11-07 12:50:18 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,33 @@
-provider "aws" {
-  region = var.aws_region
-  # credentials via env or shared profile
+locals {
+
+  region          = "eu-west-1"
+  name            = "arnab_eks_cluster"
+  node_name       = "arnab_eks_node"
+  vpc_cidr        = "10.0.0.0/16"
+  azs             = ["eu-west-1a", "eu-west-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+  intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
+  tags = {
+    Owner       = "Arnab"
+    Environment = "dev"
+  }
+
 }
 
 provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"   # for CloudFront ACM cert
+
+  region = local.region
+
 }
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
